f93640d2c968ff913e3a11b2209d84d1e92a4f1c
Mormegil <mormegil@centrum.cz>
2023-03-09 17:51:31 +0100

    WIP: Prototyping, initial screen

--- blame/src/main.ts
+++ blame/src/main.ts
@@ -6,17 +6,27 @@ enum Ordering {
     chrono = "chrono"
 };
 
+// https://www.wikidata.org/w/api.php?action=query&format=json&origin=*&prop=revisions&formatversion=2&rvprop=ids%7Ctimestamp%7Cflags%7Ccomment%7Cuser%7Cparsedcomment%7Ccontent%7Cuserid&rvslots=main&rvlimit=10&titles=Q732678
+
 let ordering = Ordering.chrono;
+let data: any = null;
 
 function init() {
     bootstrap.Dropdown.toString();
 
     document.querySelectorAll('#navComboOrdering a').forEach(elem => elem.addEventListener('click', handleOrderingChange));
+    document.querySelectorAll('button[role=search]').forEach(elem => elem.addEventListener('click', handleLoadClick));
+
+    renderScreen();
+}
+
+function handleLoadClick() {
+    data = { labels: {} };
+    renderScreen();
 }
 
 function handleOrderingChange(this: HTMLElement) {
     ordering = <Ordering>this.getAttribute('data-ordering');
-    console.log('Chosen ', ordering);
     refreshOrdering();
 }
 
@@ -28,6 +38,31 @@ function refreshOrdering() {
     const $activeItem = document.querySelector(`#navComboOrdering a[data-ordering=${ordering}]`);
     $activeItem.classList.add('active');
     $activeItem.setAttribute('aria-current', 'true');
+    renderScreen();
+}
+
+function renderScreen() {
+    const $mainContainer = document.getElementById('mainContainer');
+    if (data === null) {
+        $mainContainer.style.display = 'none';
+        document.getElementById('emptyScreen').style.display = 'block';
+    } else {
+        document.getElementById('emptyScreen').style.display = 'none';
+        $mainContainer.style.display = 'none';
+        renderData();
+        $mainContainer.style.display = 'block';
+    }
+}
+
+function renderData() {
+    renderSection('sectionContentLabelling', data.labels);
+    renderSection('sectionContentClaims', data.claims);
+    renderSection('sectionContentSitelinks', data.sitelinks);
+}
+
+function renderSection(containerId: string, labels: object) {
+    const $container = document.getElementById(containerId);
+    $container.innerHTML = '';
 }
 
 window.onload = init;
