0aa7b2c0eae0561865a7246b6b8acf78417d92fc
Mormegil <mormegil@centrum.cz>
2023-09-06 15:35:28 +0200

    Improvements

--- class-game/src/main.ts
+++ class-game/src/main.ts
@@ -14,15 +14,18 @@ let classData: WDClassesSet = undefined;
 let classIndex: string[] = undefined;
 let classNameTrie: TrieSearch<WDClassAutocomplete> = undefined;
 
-let seekedQid: string|undefined = undefined;
+let seekedQid: string | undefined = undefined;
 let gameFinished: boolean = undefined;
 let moveCounter: number = undefined;
 
 const visibleClasses = new Set<string>();
 const parentsOfVisible = new Set<string>();
 
-const treeNodes = new vis.DataSet<vis.Node>([]);
-const treeEdges = new vis.DataSet<vis.Edge>([]);
+const treeNodeIndex = new Map<string, VisTreeNode>();
+const treeEdgeIndex = new Map<string, vis.Edge>();
+
+const treeNodes = new vis.DataSet<VisTreeNode>([]);
+const treeEdges = new vis.DataSet<VisTreeEdge>([]);
 
 const treeOptions: vis.Options = {
     layout: {
@@ -37,6 +40,22 @@ const treeData: vis.Data = {
 };
 const treeNetwork = new vis.Network($('display'), treeData, treeOptions);
 treeNetwork.on('click', networkClicked);
+// treeNetwork.on('doubleClick', networkDoubleClicked);
+// TODO: Right click to remove useless nodes (?)
+
+interface VisTreeNode {
+    id: string;
+    label: string;
+    title?: string;
+    color: string;
+}
+
+interface VisTreeEdge {
+    id: string;
+    from: string,
+    to: string;
+    arrows: { to: boolean }
+}
 
 interface WDClassAutocomplete {
     qid: string;
@@ -53,41 +72,61 @@ interface WDClassesSet {
     [qid: string]: WDClassInfo;
 }
 
-function rerenderTree() {
-    treeEdges.clear();
-    treeNodes.clear();
+function updateGraph() {
+    const prevNodes = new Map<string, boolean>();
+    for (const n of treeNodes.stream()) prevNodes.set(n[1].id, false);
 
     for (const cls of visibleClasses) {
-        const node = <vis.Node>{ id: cls };
-        if (cls === seekedQid) {
-            node.label = gameFinished ? classData[cls].l : '?';
-            node.value = 20;
-            node.color = COLOR_SEEKED;
-        } else if (cls === ROOT_CLASS) {
-            node.label = classData[cls].l;
-            node.title = cls;
-            node.value = 20;
-            node.color = COLOR_ROOT;
+        if (prevNodes.has(cls) && !(gameFinished && cls === seekedQid)) {
+            prevNodes.set(cls, true);
         } else {
-            node.label = classData[cls].l;
-            node.title = cls;
-            node.value = 1;
+            const node = <VisTreeNode>{ id: cls };
+            if (cls === seekedQid) {
+                node.label = gameFinished ? classData[cls].l : '?';
+                node.color = COLOR_SEEKED;
+            } else if (cls === ROOT_CLASS) {
+                node.label = classData[cls].l;
+                node.title = cls;
+                node.color = COLOR_ROOT;
+            } else {
+                node.label = classData[cls].l;
+            }
+            if (!prevNodes.has(cls)) treeNodes.add(node);
+            else treeNodes.update(node);
+            prevNodes.set(cls, true);
         }
-        treeNodes.add(node);
     }
 
     // and now add the appropriate link for each node in the tree (except the root, obviously)
+    const prevEdges = new Map<string, boolean>();
+    for (const e of treeEdges.stream()) prevEdges.set(e[1].id, false);
+
     for (const cls of visibleClasses) {
         if (cls === ROOT_CLASS) continue;
 
         const parents = breadthFirstSearch(cls, visibleClasses);
         for (const parent of parents) {
-            treeEdges.add(<vis.Edge>{
-                from: cls,
-                to: parent,
-                arrows: { to: true }
-            });
+            const edgeId = `${cls}-${parent}`;
+            if (prevEdges.has(edgeId)) {
+                prevEdges.set(edgeId, true);
+            } else {
+                treeEdges.add(<VisTreeEdge>{
+                    id: edgeId,
+                    from: cls,
+                    to: parent,
+                    arrows: { to: true }
+                });
+            }
         }
+    }
+
+    // now remove old (removed) edges
+    for (const e of prevEdges) {
+        if (!e[1]) treeEdges.remove(e[0]);
+    }
+    // and nodes
+    for (const n of prevNodes) {
+        if (!n[1]) treeNodes.remove(n[0]);
     }
 }
 
@@ -111,7 +150,6 @@ function breadthFirstSearch(from: string, toSet: Set<string>): Set<string> {
         visited.add(curr);
         if (!classData[curr]) {
             console.error("No data for class " + curr);
-            toastError("Unexpected error! Missing data!");
             continue;
         }
         for (let parent of classData[curr].s) {
@@ -121,7 +159,6 @@ function breadthFirstSearch(from: string, toSet: Set<string>): Set<string> {
     if (!result.size) {
         // what!
         console.error("Unable to find path", from, toSet);
-        toastError("Unexpected error! Disconnected graph!");
     }
     return result;
 }
@@ -153,12 +190,12 @@ function init() {
     const $editGuess = $('editGuess') as HTMLInputElement;
     autocomplete({
         input: $editGuess,
-        fetch: function(text, update) {
+        fetch: function (text, update) {
             const results = classNameTrie.search(text);
             results.sort((a, b) => a.qidNum - b.qidNum);
             update(results);
         },
-        onSelect: function(item: WDClassAutocomplete) {
+        onSelect: function (item: WDClassAutocomplete) {
             $editGuess.value = '';
             addGuess(item);
         }
@@ -198,8 +235,12 @@ function addGuess(item: WDClassAutocomplete) {
         // done!
         gameFinished = true;
         toastInfo('Great! You have won in ' + moveCounter + ' move' + (moveCounter === 1 ? '' : 's'));
-        rerenderTree();
+        updateGraph();
         seekedQid = undefined;
+        return;
+    }
+    if (visibleClasses.has(chosenQid)) {
+        toastWarning(classData[chosenQid].l + ' is already visible!');
         return;
     }
     addAttemptedClass(chosenQid);
@@ -278,20 +319,22 @@ function startNewGame() {
     parentsOfVisible.add(ROOT_CLASS);
     addAllParentsOfVisible(seekedQid);
 
-    rerenderTree();
+    updateGraph();
 
     toastInfo("New game ready");
 }
 
 function addAllParentsOfVisible(start: string) {
+    const processed = new Set<string>();
     const stack = [start];
     while (stack.length) {
         const curr = stack.pop();
+        if (processed.has(curr)) continue;
+        processed.add(curr);
         parentsOfVisible.add(curr);
         const currClass = classData[curr];
         if (!currClass) {
             console.error("No data for class " + curr);
-            toastError("Unexpected error! Missing data!");
             continue;
         }
         for (const parent of currClass.s) {
@@ -308,7 +351,9 @@ function addAttemptedClass(chosenQid: string) {
     // and now choose _another_ class, which is the nearest parent of the chosen class and _any_ of the visible classes
     const predecessors = breadthFirstSearch(chosenQid, parentsOfVisible);
     for (const predecessor of predecessors) {
-        if (predecessor === ROOT_CLASS) {
+        if (predecessor === seekedQid) {
+            // oops, we don’t want to show this!
+        } else if (predecessor === ROOT_CLASS) {
             if (predecessors.size === 1) {
                 console.debug("Oops, no common path to root!");
             }
@@ -324,18 +369,41 @@ function addAttemptedClass(chosenQid: string) {
     }
     */
     addAllParentsOfVisible(chosenQid);
-    rerenderTree();
+    updateGraph();
 }
 
 function addHint() {
-    // TODO: Find the furthest not-yet visible “parent” of the seeked class and add it
-    
-    let chosenQid: string;
-    do {
-        const idx = Math.floor(classIndex.length * Math.random());
-        chosenQid = classIndex[idx];
-    } while (visibleClasses.has(chosenQid));
-    addAttemptedClass(chosenQid);
+    const queue = [seekedQid];
+    let queuePos = 0;
+    const sortedParents = [];
+
+    // Find the furthest not-yet visible “parent” of the seeked class and add it
+    while (queuePos < queue.length) {
+        const currQid = queue[queuePos++];
+        const data = classData[currQid];
+        if (!data) {
+            console.error('No data for', currQid);
+            continue;
+        }
+        for (const parent of data.s) {
+            if (visibleClasses.has(parent)) {
+                if (currQid !== seekedQid) sortedParents.push(currQid);
+                continue;
+            }
+            queue.push(parent);
+        }
+    }
+
+    if (sortedParents.length) {
+        const chosenQid = sortedParents[sortedParents.length - 1];
+        addAttemptedClass(chosenQid);
+        toastInfo('See ' + classData[chosenQid].l);
+        return;
+    }
+
+    // Oops… so… find the nearest not-yet visible “sibling” of the seeked class and add that
+
+    toastWarning("Sorry, there is nothing to help you with");
 }
 
 function networkClicked(evt: any) {
@@ -352,6 +420,7 @@ function networkClicked(evt: any) {
 }
 
 function validateClassData() {
+    let hasProblem = false;
     const missing = new Map<string, string[]>();
     for (const qid in classData) {
         const data = classData[qid];
@@ -369,10 +438,12 @@ function validateClassData() {
         }
         if (!data.s.length && qid !== ROOT_CLASS) {
             console.error("Class", qid, "has no parents!");
+            hasProblem = true;
         }
     }
     for (const missingCls of missing.entries()) {
         console.error("Parent class", missingCls[0], "is missing, required by", missingCls[1].join(', '));
+        hasProblem = true;
     }
 
     console.debug("Finding cycles...")
@@ -383,7 +454,11 @@ function validateClassData() {
         console.error("Cycles found!");
         for (const cycle of cycleSet) {
             console.log(cycle);
+            hasProblem = true;
         }
+    }
+    if (hasProblem) {
+        toastError("Detected problems in class data");
     }
 }
 
